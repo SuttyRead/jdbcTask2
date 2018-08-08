209558128379fd1fddfd2ab897bd16cfd58639e9
Anton Kabatut <anton.kabatut@nixsolutions.com>
2018-08-08 21:19:56 +0300

    refactor code

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,20 @@ import dao.JdbcUserDao;
 import entity.Role;
 import entity.User;
 
+import java.sql.Date;
 import java.util.Arrays;
-import java.util.Date;
+
 
 public class Main {
 
     public static void main(String[] args) {
         JdbcRoleDao roleDao = new JdbcRoleDao();
         JdbcUserDao userDao = new JdbcUserDao();
-        Role role = new Role(3L, "Guest");
+        Role role = new Role(5L, "Guest");
         roleDao.create(role);
-        userDao.create(new User(5L, "user5", "root", "user5@email.com", "Rich", "Green",
+        userDao.create(new User(8L, "user5", "root", "user5@email.com", "Rich", "Green",
                 new Date(System.currentTimeMillis()), role));
-        userDao.create(new User(6L, "user6", "root", "user6@email.com", "Rich", "Black",
+        userDao.create(new User(9L, "user6", "root", "user6@email.com", "Rich", "Black",
                 new Date(System.currentTimeMillis()), role));
         System.out.println(Arrays.toString(userDao.findAll().toArray()));
         System.out.println(Arrays.toString(roleDao.findAll().toArray()));
